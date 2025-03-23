446ca6b309b47b775b73803e1f92dd4781320ed1
StarlessDev <contact@starless.dev>
2025-03-23 18:06:54 +0100

    Removed protobuf dependency from PatEvent

--- src/main/java/com/fabiodm/pat/api/event/PatEvent.java
+++ src/main/java/com/fabiodm/pat/api/event/PatEvent.java
@@ -1,8 +1,5 @@
 package com.fabiodm.pat.api.event;
 
-import com.google.protobuf.InvalidProtocolBufferException;
-import com.google.protobuf.Parser;
-
 import java.util.Optional;
 
 /**
@@ -12,22 +9,6 @@ import java.util.Optional;
 public record PatEvent(String channel, byte[] message) {
 
     /**
-     * Serializes the message into a Protocol Buffers message.
-     * If the serialization fails, an empty Optional is returned.
-     *
-     * @param parser the Protocol Buffers parser to use for serialization
-     * @param <T>    the type of the Protocol Buffers message
-     * @return an Optional containing the serialized message if successful, otherwise an empty Optional
-     */
-    public <T> Optional<T> serializeProto(final Parser<T> parser) {
-        try {
-            return Optional.ofNullable(parser.parseFrom(this.message));
-        } catch (final InvalidProtocolBufferException e) {
-            return Optional.empty();
-        }
-    }
-
-    /**
      * Returns the message as a String.
      * If the message is null, null is returned.
      *
@@ -35,7 +16,7 @@ public record PatEvent(String channel, byte[] message) {
      */
     public String messageString() {
         return Optional.ofNullable(this.message)
-            .map(String::new)
-            .orElse(null);
+                .map(String::new)
+                .orElse(null);
     }
 }
