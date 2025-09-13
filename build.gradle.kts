238f1cc830f2d47ecac9ab1a060272b9da6c5591
StarlessDev <contact@starless.dev>
2025-09-13 16:53:35 +0200

    build: compile to java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,3 +35,11 @@ publishing {
         }
     }
 }
+
+tasks {
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+}
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
