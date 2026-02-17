de15038e8c21159b53b4292a28f8c8027224bc72
StarlessDev <contact@starless.dev>
2026-02-17 13:40:49 +0100

    build: update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,10 +36,14 @@ publishing {
     }
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 tasks {
     compileJava {
         options.encoding = Charsets.UTF_8.name()
         options.release.set(17)
     }
 }
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
